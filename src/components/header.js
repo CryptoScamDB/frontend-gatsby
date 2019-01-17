78b04638dc6d85b350ecc0a6e205c67043d861f2
409H <409H@users.noreply.github.com>
2019-01-17 16:45:17 +0000

    Initial commit

---
+++ src/components/header.js
@@ -0,0 +1,35 @@
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+import Image from '../components/image'
+
+const Header = ({ siteTitle, siteLogo }) => (
+  <div id="header">
+    <div id="header--container">
+      <div id="header--container-brand">
+        <Link to="/">
+          <img src="icons/icon-144x144.png" />
+        </Link>
+      </div>
+      <div id="header--container-navigation">
+        <ul>
+          <li><Link to="/scams">See Scams</Link></li>
+          <li><Link to="/verified">Verified Domains</Link></li>
+          <li><Link to="/api">API</Link></li>
+          <li><Link to="/faq">FAQ</Link></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+  siteLogo: `images/logo/csdb-logo.svg`
+}
+
+export default Header
---
+++ src/components/layout.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
+
+import Header from './header'
+import '../css/layout.scss'
+
+const Layout = ({ id, children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <div id={id}>
+          <Header siteTitle={data.site.siteMetadata.title} />
+            <div id={id +"--container"}>
+              {children}
+              <footer>
+                
+              </footer>
+            </div>
+        </div>
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
