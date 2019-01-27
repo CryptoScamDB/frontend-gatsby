9b0e503dd9de87278918a23daafff570deecae9b
409H <409H@users.noreply.github.com>
2019-01-27 21:50:11 +0000

    Added new views

---
+++ src/pages/faq.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+//import Table from '../components/table'
+
+const FaqPage = () => (
+  <Layout id="faq-view">
+    <SEO title="FAQ" keywords={[`ethereum`,`faq`,`mycrypto`]} />
+
+    <h1>FAQ</h1>
+    
+  </Layout>
+)
+
+export default FaqPage
---
+++ src/pages/verified.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+//import Table from '../components/table'
+
+const VerifiedPage = () => (
+  <Layout id="verified-view">
+    <SEO title="Verified Domains" keywords={[`ethereum`,`verified`,`mycrypto`]} />
+
+    <h1>Verified Domains</h1>
+    
+  </Layout>
+)
+
+export default VerifiedPage
