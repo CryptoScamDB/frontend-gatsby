1c72bb9c7a28e6ab0face2fe1e4c4c73afe95ca6
409H <409H@users.noreply.github.com>
2019-01-27 21:49:21 +0000

    Made links have tab indexes, linked off api to external

--- src/components/header.js
+++ src/components/header.js
@@ -13,10 +13,10 @@ const Header = ({ siteTitle, siteLogo }) => (
       </div>
       <div id="header--container-navigation">
         <ul>
-          <li><Link to="/scams">See Scams</Link></li>
-          <li><Link to="/verified">Verified Domains</Link></li>
-          <li><Link to="/api">API</Link></li>
-          <li><Link to="/faq">FAQ</Link></li>
+          <li><Link to="/scams" role="link" tabIndex="1">See Scams</Link></li>
+          <li><Link to="/verified" role="link" tabIndex="2">Verified Domains</Link></li>
+          <li><a href="https://api.cryptoscamdb.org" role="link" tabIndex="3">API</a></li>
+          <li><Link to="/faq" role="link" tabIndex="4">FAQ</Link></li>
         </ul>
       </div>
     </div>
