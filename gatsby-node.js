596df7a0c668a6ed5f68b4c0e75b602a7ecbc920
409H <409H@users.noreply.github.com>
2019-01-30 23:41:52 +0000

    Fixed node issue that only caused 80 to be created

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,41 +9,39 @@ const {
     typePrefix: "Csdb"
 })
 
-const DataNode = createNodeFactory('Data');
 const DomainNode = createNodeFactory('Domains');
 
- exports.sourceNodes = async ({ actions: { createNode } }) => {
+exports.sourceNodes = async ({ actions: { createNode } }) => {
 
-    const objScams = await axios.get("https://api.cryptoscamdb.org/v1/scams");
+const objScams = await axios.get("https://api.cryptoscamdb.org/v1/scams");
 
-    if(objScams.data.success) {
-        await Promise.all(
-            objScams.data.result.map(async data => {
-                if(data.name) {
-                    const objResponse = await axios.get(`https://api.cryptoscamdb.org/v1/domain/${data.name}`);
-                    if(objResponse.data && objResponse.data.success) {
-                        const objLookup = objResponse.data.result;
+const objBuildStats = {
+    "success": 0,
+    "fail": 0
+};
 
-                        data = objLookup.map(lookup => {
-                            const objData = DataNode(lookup, {
-                                parent: generateNodeId("DomainNode", data.id)
-                            });
-                            
-                            createNode(objData);
-
-                            return objData;
-                        });
-                    } else {
-                        console.log(`\r\nBUILD ERROR: Cannot get specific scam: ${data.name}`);
-                    }
+if(objScams.status === 200 && objScams.data.success) {
+    await Promise.all(
+        objScams.data.result.map(async data => {
+            if(data.name) {
+                const objResponse = await axios.get(`https://api.cryptoscamdb.org/v1/domain/${data.name}`);
+                if(objResponse.status === 200 && objResponse.data && objResponse.data.success) {
+                    objBuildStats.success += 1;
+                    createNode(DomainNode(objLookup));
+                } else {
+                    console.log(`\r\nBUILD ERROR: Cannot get specific scam: ${data.name} -- HTTP: ${objResponse.status}`);
+                    objBuildStats.fail += 1;
                 }
+            }
 
-                const objDomainNode = DomainNode(data);
-                createNode(objDomainNode);         
-            })
-        );
-    } else {
-        console.log("\r\nBUILD ERROR: Cannot get scams");
-        console.log(objScams);
-    } 
- }
+            const objDomainNode = DomainNode(data);
+            createNode(objDomainNode);
+        })
+    );
+} else {
+    console.log("\r\nBUILD ERROR: Cannot get scams");
+    console.log(objScams);
+} 
+
+console.log(`-------\r\nBUILD STATS\r\n-------\r\nSuccess:${objBuildStats.success}\r\nFail:${objBuildStats.fail}\r\n\r\nFinished.`)
+}
