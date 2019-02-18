a960fd1e5d8b84160bc63b181219697e49914e32
409H <409H@users.noreply.github.com>
2019-02-18 22:47:35 +0000

    Added logic to build nodes for bad domains then create pages using the domains.js template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,7 @@
+const CSDB_API_ENDPOINT = "https://api.cryptoscamdb.org/v1";
+const DEVELOPMENT = true; // Will limit scam lookups to 100 instead of everything
+
+const path = require(`path`)
 const createNodeHelpers = require("gatsby-node-helpers").default;
 const axios = require('axios');
 
@@ -11,37 +15,74 @@ const {
 
 const DomainNode = createNodeFactory('Domains');
 
-exports.sourceNodes = async ({ actions: { createNode } }) => {
+exports.sourceNodes = async ({ actions: { createNode } }) => 
+{
+    const objScams = await axios.get(`${CSDB_API_ENDPOINT}/scams`);
 
-const objScams = await axios.get("https://api.cryptoscamdb.org/v1/scams");
+    const objBuildStats = {
+        "success": 0,
+        "fail": 0
+    };
 
-const objBuildStats = {
-    "success": 0,
-    "fail": 0
-};
+    if(objScams.status === 200 && objScams.data.success) {
 
-if(objScams.status === 200 && objScams.data.success) {
-    await Promise.all(
-        objScams.data.result.map(async data => {
-            if(data.name) {
-                const objResponse = await axios.get(`https://api.cryptoscamdb.org/v1/domain/${data.name}`);
-                if(objResponse.status === 200 && objResponse.data && objResponse.data.success) {
-                    objBuildStats.success += 1;
-                    createNode(DomainNode(objLookup));
-                } else {
-                    console.log(`\r\nBUILD ERROR: Cannot get specific scam: ${data.name} -- HTTP: ${objResponse.status}`);
-                    objBuildStats.fail += 1;
+        let arrResults = objScams.data.result;
+        if(DEVELOPMENT) {
+            console.log("\r\n\r\nDEVELOPMENT MODE SO ONLY GETTING THE MOST RECENT 100 RECORDS\r\n\r\n");
+            arrResults = objScams.data.result.reverse().slice(1,100);
+        }
+
+        await Promise.all(
+            arrResults.map(async data => {
+                if(data.name) {
+                    const objResponse = await axios.get(`${CSDB_API_ENDPOINT}/domain/${data.name.toLowerCase()}`);
+                    if(objResponse.status === 200 && objResponse.data && objResponse.data.success) {
+                        objBuildStats.success += 1;
+                        createNode(DomainNode(objResponse.data.result));
+                    } else {
+                        console.log(`\r\nBUILD ERROR: Cannot get specific scam: ${data.name} -- HTTP: ${objResponse.status}`);
+                        objBuildStats.fail += 1;
+                    }
                 }
-            }
 
-            const objDomainNode = DomainNode(data);
-            createNode(objDomainNode);
-        })
-    );
-} else {
-    console.log("\r\nBUILD ERROR: Cannot get scams");
-    console.log(objScams);
-} 
+                const objDomainNode = DomainNode(data);
+                createNode(objDomainNode);
+            })
+        );
+    } else {
+        console.log("\r\nBUILD ERROR: Cannot get scams");
+    } 
+    console.log(`-------\r\nBUILD STATS\r\n-------\r\nSuccess:${objBuildStats.success}\r\nFail:${objBuildStats.fail}\r\n\r.`)
+}
 
-console.log(`-------\r\nBUILD STATS\r\n-------\r\nSuccess:${objBuildStats.success}\r\nFail:${objBuildStats.fail}\r\n\r\nFinished.`)
+exports.createPages = ({ graphql, actions} ) => 
+{
+    const { createPage } = actions;
+    return new Promise((resolve, reject) => {
+        resolve(
+            graphql(`
+                {
+                    allCsdbDomains {
+                        totalCount
+                        edges {
+                            node {
+                                csdbId
+                            }
+                        } 
+                    }
+                }
+            `).then(result => {
+                result.data.allCsdbDomains.edges.forEach(({ node }) => {
+                    console.log(`\tCreating page /domain/${node.csdbId}`)
+                    createPage({
+                        path: `/domain/${node.csdbId}`,
+                        component: path.resolve(`./src/pages/domain.js`),
+                        context: {
+                            slug: node.csdbId
+                        }
+                    })
+                })
+            })
+        )
+    })
 }
