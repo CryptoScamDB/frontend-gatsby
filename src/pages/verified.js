e85e005a100ce42dd8bba2f3bf8d7103965d024c
409H <409H@users.noreply.github.com>
2019-02-26 21:54:58 +0000

    Added verified table fetched from graphql

--- src/pages/verified.js
+++ src/pages/verified.js
@@ -1,17 +1,50 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
+import { graphql, Link } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-//import Table from '../components/table'
 
-const VerifiedPage = () => (
-  <Layout id="verified-view">
-    <SEO title="Verified Domains" keywords={[`ethereum`,`verified`,`mycrypto`]} />
+export default function VerifiedPage({data}) {
+    return (
+    <Layout id="verified-view">
+        <SEO title="Verified Domains" keywords={[`ethereum`,`verified`,`mycrypto`]} />
 
-    <h1>Verified Domains</h1>
-    
-  </Layout>
-)
+        <h2 id="heading">Verified Domains</h2>
 
-export default VerifiedPage
+        <br />
+        
+        <table>
+            <thead>
+                <th>Name</th>
+                <th>URL</th>
+                <th>Description</th>
+            </thead>
+            <tbody>
+            {data.allCsdbFeaturedDomain.edges.map(domain => {
+                  return(
+                    <tr key={domain.node.name}>
+                        <td>{domain.node.name}</td>
+                        <td>{domain.node.url}</td>
+                        <td>{domain.node.description}</td>
+                    </tr>
+                )
+            })}
+            </tbody>
+        </table>
+      </Layout> 
+    )
+}
+
+export const pageQuery = graphql`
+    query GetVerifiedDomains {
+        allCsdbFeaturedDomain {
+            totalCount
+            edges {
+                node {
+                    name
+                    url
+                    description
+                }
+            }
+        }
+    }
+`
