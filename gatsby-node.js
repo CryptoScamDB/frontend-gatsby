093d4980d5ed3ca3fd36081152fbfd47fcd3d1f6
409H <409H@users.noreply.github.com>
2019-02-26 21:33:37 +0000

    Added logic to get the stats and featured/verified domains. Rearranged some logic to make it cleaner

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,10 +13,16 @@ const {
     typePrefix: "Csdb"
 })
 
-const DomainNode = createNodeFactory('Domains');
+const ScamDomainNode = createNodeFactory('ScamDomains');
+const FeaturedDomainNode = createNodeFactory('FeaturedDomain');
+const StatsNode = createNodeFactory('Stats');
 
 exports.sourceNodes = async ({ actions: { createNode } }) => 
 {
+    /***************************************************************
+     ********************** G E T   S C A M S **********************
+     ***************************************************************/
+    console.log(`\r\n\r\n[*] Fetching domains -- ${CSDB_API_ENDPOINT}/scams`);
     const objScams = await axios.get(`${CSDB_API_ENDPOINT}/scams`);
 
     const objBuildStats = {
@@ -25,10 +31,10 @@ exports.sourceNodes = async ({ actions: { createNode } }) =>
     };
 
     if(objScams.status === 200 && objScams.data.success) {
-
         let arrResults = objScams.data.result;
+
         if(DEVELOPMENT) {
-            console.log("\r\n\r\nDEVELOPMENT MODE SO ONLY GETTING THE MOST RECENT 100 RECORDS\r\n\r\n");
+            console.log(`\r\n\t[+] Development mode so only getting the most recent 100 records`);
             arrResults = objScams.data.result.reverse().slice(1,100);
         }
 
@@ -38,21 +44,68 @@ exports.sourceNodes = async ({ actions: { createNode } }) =>
                     const objResponse = await axios.get(`${CSDB_API_ENDPOINT}/domain/${data.name.toLowerCase()}`);
                     if(objResponse.status === 200 && objResponse.data && objResponse.data.success) {
                         objBuildStats.success += 1;
-                        createNode(DomainNode(objResponse.data.result));
+                        createNode(ScamDomainNode(objResponse.data.result[0]));
                     } else {
-                        console.log(`\r\nBUILD ERROR: Cannot get specific scam: ${data.name} -- HTTP: ${objResponse.status}`);
+                        console.log(`\r\n\t[-] Build error - cannot get domain: ${data.name}`);
                         objBuildStats.fail += 1;
                     }
                 }
-
-                const objDomainNode = DomainNode(data);
-                createNode(objDomainNode);
             })
         );
     } else {
-        console.log("\r\nBUILD ERROR: Cannot get scams");
+        console.log(`\r\n\t[-] Build error - cannot get ${CSDB_API_ENDPOINT}/scams`);
     } 
-    console.log(`-------\r\nBUILD STATS\r\n-------\r\nSuccess:${objBuildStats.success}\r\nFail:${objBuildStats.fail}\r\n\r.`)
+    console.log(`\r\n\t[+] Successful build of scams: ${JSON.stringify(objBuildStats)}`);
+
+    /***************************************************************
+     ******************* G E T   V E R I F E D *********************
+     ***************************************************************/
+    console.log(`\r\n\r\n[+] Fetching CSDB verified/featured`);
+    const objFeatured = await axios.get(`${CSDB_API_ENDPOINT}/featured`);
+
+    if(objFeatured.status === 200 && objFeatured.data.success) {
+        let arrResults = objFeatured.data.result;
+        await Promise.all(
+            arrResults.map(async data => {
+                if(data.url) {
+                    let strDomain = data.url.replace(/https?\:\/\//, '');
+                    const objResponse = await axios.get(`${CSDB_API_ENDPOINT}/domain/${strDomain.toLowerCase()}`);
+                    if(objResponse.status === 200 && objResponse.data && objResponse.data.success) {
+                        objBuildStats.success += 1;
+
+                        //Add the CSDB ID to it - hack until included in the GET /v1/domain/<domain>
+                        objResponse.data.result[0].csdbId = data.csdbId;
+                        // Normalise the record @todo make this much prettier, maybe using a data model?
+                        if(!objResponse.data.result[0].hasOwnProperty("description")) {
+                            objResponse.data.result[0].description = "";
+                        }
+                        createNode(FeaturedDomainNode(objResponse.data.result[0]))
+                        console.log(objResponse.data.result[0])
+                    } else {
+                        console.log(`\r\n\t[-] Build error - cannot get domain: ${strDomain}`);
+                        objBuildStats.fail += 1;
+                    }
+                }
+            })
+        );
+        console.log(`\r\n\t[+] Verified domains node created`);
+    } else {
+        console.log(`\r\n\t[-] Build error - cannot get ${CSDB_API_ENDPOINT}/featured`);
+    }
+
+    /***************************************************************
+     ********************** G E T   S T A T S **********************
+     ***************************************************************/
+    console.log(`\r\n\r\n[+] Fetching CSDB stats`);
+    const objStats = await axios.get(`${CSDB_API_ENDPOINT}/stats`);
+
+    if(objStats.status === 200 && objStats.data.success) {
+        let arrResults = objStats.data.result;
+        createNode(StatsNode(arrResults))
+        console.log(`\r\n\t[+] Stats node created`);
+    } else {
+        console.log(`\r\n\t[-] Build error - cannot get ${CSDB_API_ENDPOINT}/stats`);
+    }
 }
 
 exports.createPages = ({ graphql, actions} ) => 
@@ -62,7 +115,7 @@ exports.createPages = ({ graphql, actions} ) =>
         resolve(
             graphql(`
                 {
-                    allCsdbDomains {
+                    allCsdbScamDomains {
                         totalCount
                         edges {
                             node {
@@ -72,7 +125,7 @@ exports.createPages = ({ graphql, actions} ) =>
                     }
                 }
             `).then(result => {
-                result.data.allCsdbDomains.edges.forEach(({ node }) => {
+                result.data.allCsdbScamDomains.edges.forEach(({ node }) => {
                     console.log(`\tCreating page /domain/${node.csdbId}`)
                     createPage({
                         path: `/domain/${node.csdbId}`,
