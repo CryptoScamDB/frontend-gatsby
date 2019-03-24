5ed299b54367929f958591103b5eaed5ecdc4d5e
409H <409H@users.noreply.github.com>
2019-03-24 15:07:14 +0000

    Fixed bug in screenshot that didn't let you see the first image

--- src/components/domain/ScreenshotArea.tsx
+++ src/components/domain/ScreenshotArea.tsx
@@ -9,10 +9,10 @@ import Thumbnail from './Thumbnail';
  * The container for the screenshot area
  */
 const ScreenshotContainer = styled.div`
-    background: #001D2F;
-    height: 600px;
-    display: flex;
-`
+  background: #001d2f;
+  height: 600px;
+  display: flex;
+`;
 
 /**
  * The container for the left/right arrows
@@ -29,209 +29,192 @@ const LeftAndRightArrows = styled.div`
     -webkit-touch-callout: none;
     -o-user-select: none;
     -moz-user-select: none;
-`
+`;
 
 /**
  * The container for the left arrow
  */
 const LeftArrow = styled.div`
-    width: 100%;
-    height: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-top: 1px solid #001D2F;
+  width: 100%;
+  height: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-top: 1px solid #001d2f;
 
-    &:hover {
-        cursor: pointer;
+  &:hover {
+    cursor: pointer;
 
-        img {
-            filter: brightness(0) sepia(1) hue-rotate(-70deg) saturate(5)
-        }
+    img {
+      filter: brightness(0) sepia(1) hue-rotate(-70deg) saturate(5);
     }
-`
+  }
+`;
 
 /**
  * The container for the right arrow
  */
 const RightArrow = styled.div`
-    width: 100%;
-    height: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-bottom: 1px solid #001D2F;
+  width: 100%;
+  height: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid #001d2f;
 
-    &:hover {
-        cursor: pointer;
+  &:hover {
+    cursor: pointer;
 
-        img {
-            filter: brightness(0) sepia(1) hue-rotate(-70deg) saturate(5)
-        }
+    img {
+      filter: brightness(0) sepia(1) hue-rotate(-70deg) saturate(5);
     }
-`
+  }
+`;
 
 /**
  * The container for the screenshots
  */
 const ScreenshotImages = styled.div`
-    background: #FFF;
-    flex: 1;
-    width: 100%;
+  background: #fff;
+  flex: 1;
+  width: 100%;
 
-    user-select: none;
-    -webkit-user-select: none;
-    -ms-user-select: none;
-    -webkit-touch-callout: none;
-    -o-user-select: none;
-    -moz-user-select: none;
-`
+  user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  -webkit-touch-callout: none;
+  -o-user-select: none;
+  -moz-user-select: none;
+`;
 
 const NoScreenshot = styled.div`
-    color: #001D2F;
-    font-size: 22pt;
-    text-align: center;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
+  color: #001d2f;
+  font-size: 22pt;
+  text-align: center;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 /**
  * The container for the thumbnails
  */
 const Thumbnails = styled.div`
-    background: #001D2F;
-    display: flex;
-    flex-wrap: wrap;
-    flex: 1;
-    padding: 0.5em;
-`
+  background: #001d2f;
+  display: flex;
+  flex-wrap: wrap;
+  flex: 1;
+  padding: 0.5em;
+`;
 
 interface Props {
-    images: any[];
+  images: any[];
 }
 
 interface State {
-    currentImage: number;
-    totalImages: number;
+  currentImage: number;
+  totalImages: number;
 }
 
 export default class ScreenshotArea extends Component<Props, State> {
-    static defaultProps = {
-        images: []
-    }
-    
-    constructor(props: Props) {
-        super(props);
-    
-        this.nextImage = this.nextImage.bind(this);
-        this.prevImage = this.prevImage.bind(this);
-        this.jumpToImage = this.jumpToImage.bind(this);
+  static defaultProps = {
+    images: []
+  };
 
-        this.state = {
-            currentImage: 1,
-            totalImages: this.props.images.length
-        }
-    }
+  constructor(props: Props) {
+    super(props);
 
-    /**
-     * Sets the state for the next image in the cycle
-     */
-    nextImage() {
-        const { currentImage, totalImages } = this.state;
-        const nextImage = currentImage + 1 > totalImages 
-            ? 1 
-            : currentImage + 1;
-        this.setState({ currentImage: nextImage });
-    }
+    this.nextImage = this.nextImage.bind(this);
+    this.prevImage = this.prevImage.bind(this);
+    this.jumpToImage = this.jumpToImage.bind(this);
 
-    /**
-     * Sets the state for the previous image in the cycle
-     */
-    prevImage() {
-        const { currentImage, totalImages } = this.state;
-        const nextImage = currentImage - 1 < 1 
-            ? totalImages 
-            : currentImage - 1;
-        this.setState({ currentImage: nextImage });
-    }
+    this.state = {
+      currentImage: 0,
+      totalImages: this.props.images.length - 1
+    };
+  }
 
-    jumpToImage(event: MouseEvent<HTMLElement>) {
-        const { totalImages } = this.state;
-        const { target } = event;
-        const { index } = (target as HTMLElement).dataset;
-        const intJumpTo = parseInt((index && typeof index !== 'undefined') ? index : '1', 10);
-        if(intJumpTo > 0 && intJumpTo <= totalImages) {
-            this.setState({ currentImage: intJumpTo });
-        }
-    }
+  /**
+   * Sets the state for the next image in the cycle
+   */
+  nextImage() {
+    const { currentImage, totalImages } = this.state;
+    const nextImage = currentImage + 1 > totalImages ? 0 : currentImage + 1;
 
-    /**
-     * Renders the screenshot area component
-     */
-    render() {
-        const { currentImage } = this.state;
-        const { images } = this.props;
-        return (
-            <div>
-                <ScreenshotContainer>
-                    <LeftAndRightArrows>
-                        <RightArrow onClick={this.nextImage}>
-                            <img alt="Next" src={Right} />
-                        </RightArrow>
-                        <LeftArrow onClick={this.prevImage}>
-                            <img alt="Previous" src={Left} />
-                        </LeftArrow>
-                    </LeftAndRightArrows>
-                    <ScreenshotImages>
-                        {
-                            images.length
-                                ?
-                                    images.map((r, index) => 
-                                        <Screenshot 
-                                            image={r} 
-                                            alt="Screenshot" 
-                                            index={index.toString()} 
-                                            key={index}
-                                            active={
-                                                currentImage === index 
-                                                    ? true 
-                                                    : false
-                                            }
-                                        />
-                                    )
-                                :
-                                    <NoScreenshot>No screenshots available</NoScreenshot>
-                            
-                        }
-                    </ScreenshotImages>
-                </ScreenshotContainer>
-                {
-                    images.length
-                        ?
-                            <Thumbnails>
-                                {
-                                    images.map((r, index) => 
-                                        <Thumbnail 
-                                            image={r} 
-                                            alt="Screenshot" 
-                                            index={index.toString()} 
-                                            key={index}
-                                            active={
-                                                currentImage === index 
-                                                    ? true 
-                                                    : false
-                                            }
-                                            onClick={this.jumpToImage}
-                                        />
-                                    )
-                                }
-                            </Thumbnails>
-                        :
-                            ``
-                }
-            </div>
-        )
+    this.setState({ currentImage: nextImage });
+  }
+
+  /**
+   * Sets the state for the previous image in the cycle
+   */
+  prevImage() {
+    const { currentImage, totalImages } = this.state;
+    const nextImage = currentImage - 1 < 0 ? totalImages : currentImage - 1;
+
+    this.setState({ currentImage: nextImage });
+  }
+
+  jumpToImage(event: MouseEvent<HTMLElement>) {
+    const { totalImages } = this.state;
+    const { target } = event;
+    const { index } = (target as HTMLElement).dataset;
+    const intJumpTo = parseInt(index && typeof index !== 'undefined' ? index : '1', 10);
+    if (intJumpTo >= 0 && intJumpTo <= totalImages) {
+      this.setState({ currentImage: intJumpTo });
     }
+  }
+
+  /**
+   * Renders the screenshot area component
+   */
+  render() {
+    const { currentImage } = this.state;
+    const { images } = this.props;
+    return (
+      <div>
+        <ScreenshotContainer>
+          <LeftAndRightArrows>
+            <RightArrow onClick={this.nextImage}>
+              <img alt="Next" src={Right} />
+            </RightArrow>
+            <LeftArrow onClick={this.prevImage}>
+              <img alt="Previous" src={Left} />
+            </LeftArrow>
+          </LeftAndRightArrows>
+          <ScreenshotImages>
+            {images.length ? (
+              images.map((r, index) => (
+                <Screenshot
+                  image={r}
+                  alt="Screenshot"
+                  index={index.toString()}
+                  key={index}
+                  active={currentImage === index ? true : false}
+                />
+              ))
+            ) : (
+              <NoScreenshot>No screenshots available</NoScreenshot>
+            )}
+          </ScreenshotImages>
+        </ScreenshotContainer>
+        {images.length ? (
+          <Thumbnails>
+            {images.map((r, index) => (
+              <Thumbnail
+                image={r}
+                alt="Screenshot"
+                index={index.toString()}
+                key={index}
+                active={currentImage === index ? true : false}
+                onClick={this.jumpToImage}
+              />
+            ))}
+          </Thumbnails>
+        ) : (
+          ``
+        )}
+      </div>
+    );
+  }
 }
