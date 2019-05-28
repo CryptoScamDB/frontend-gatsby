9baef9359312e6f09fd32f4bc878ac29100a1dd8
409H <409H@users.noreply.github.com>
2019-05-28 01:18:30 +0100

    Added csdb privacy policy segment

--- src/pages/mission.tsx
+++ src/pages/mission.tsx
@@ -8,6 +8,10 @@ const SubHeading = styled.h2`
 `;
 const MissionBlock = styled.p`
   padding: 0 5em;
+
+  > ul {
+    padding-left: 2em;
+  }
 `;
 
 const MISSION_PAGE: React.StatelessComponent = () => (
@@ -59,6 +63,98 @@ const MISSION_PAGE: React.StatelessComponent = () => (
       as is required by law. We utilize a minimal, self-hosted analytics platform to help us improve
       our product, including which features people use and how many views pages have, but we do not
       collect any personally-identifiable information from visitors of our products.
+    </MissionBlock>
+
+    <br />
+
+    <MissionBlock>
+      <a href="https://etherscamdb.info" target="_blank" rel="noreferrer">
+        EtherScamDB.info
+      </a>{' '}
+      and{' '}
+      <a href="https://cryptoscamdb.org" target="_blank" rel="noreferrer">
+        CryptoScamDB.org
+      </a>{' '}
+      were created to gather and analyze data that can be shared with various projects, law
+      enforcement agencies, and related organizations to prevent and terminate phishing, theft, and
+      scams, assist in identifying and potentially leading to the arrest of these scams’ operators,
+      as well as generally protecting users in the cryptocurrency ecosystem.
+    </MissionBlock>
+    <MissionBlock>
+      <a href="https://etherscamdb.info" target="_blank" rel="noreferrer">
+        EtherScamDB.info
+      </a>{' '}
+      and{' '}
+      <a href="https://cryptoscamdb.org" target="_blank" rel="noreferrer">
+        CryptoScamDB.org
+      </a>{' '}
+      are unique from other MyCrypto products in that they offer a "report" functionality. This
+      allows any person to make a report (collectively these are the "Raw Reports") regarding items
+      such as suspicious website URLs, suspicious cryptocurrency addresses, and scams being
+      propagated via social media, or to report that their own cryptocurrencies have been stolen.
+    </MissionBlock>
+    <MissionBlock>
+      Depending on the circumstances, people using the "report" functionality may be prompted to
+      provide:
+      <ul>
+        <li>Their cryptocurrency public address</li>
+        <li>The cryptocurrency public address they sent funds to</li>
+        <li>Website(s) visited</li>
+        <li>Suspicious website URLs</li>
+        <li>Suspicious cryptocurrency public addresses</li>
+        <li>Additional information the user wishes to share</li>
+      </ul>
+    </MissionBlock>
+    <MissionBlock>
+      These Raw Reports are sent and stored in a MyCrypto communications channel, and some of the
+      data is mirrored to a MongoDB server hosted by mLab. These reports are generally not
+      immediately deleted but marked as processed. We aim to destroy any reports containing
+      sensitive or personal information as soon as possible. However, there are instances when
+      deletion may take longer than expected.
+    </MissionBlock>
+    <MissionBlock>
+      These reports may be kept in digital form, as permitted by law, and kept in a manner that is
+      reasonably necessary for the CryptoScamDB team to perform its business operations effectively.
+      The information gathered via the Raw Reports is used to create and maintain various databases
+      of aggregate information (the "Aggregate Data").
+    </MissionBlock>
+    <MissionBlock>
+      The Raw Reports are normally not shared with, nor sold to any third party without the explicit
+      written permission of the reporter. However, Raw Reports may be shared with a third party as
+      is required under applicable laws or regulations, or as is deemed necessary by MyCrypto to
+      best fulfill its goal of protecting the safety and security of Users across the cryptocurrency
+      space.
+    </MissionBlock>
+    <MissionBlock>
+      The Aggregate Data, analyses of suspicious website URLs, and analyses of suspicious
+      cryptocurrency public addresses are published to a public database that is accessible by
+      anyone via:
+      <ul>
+        <li>https://etherscamdb.info;</li>
+        <li>https://cryptoscamdb.org;</li>
+        <li>https://api.cryptoscamdb.org;</li>
+        <li>https://github.com/cryptoscamdb;</li>
+        <li>https://github.com/MrLuit/EtherScamDB;</li>
+      </ul>
+      or related forks of these repositories.
+    </MissionBlock>
+    <MissionBlock>
+      These website URLs and public addresses are sourced in part from the Raw Reports. MyCrypto
+      takes reasonable measures to confirm the accuracy and validity of the reports, but does not
+      accept responsibility nor liability for the information in these databases. Any person or
+      entity accessing these reports should treat them as public information, and should take
+      additional measures to confirm the information contained in the databases.
+    </MissionBlock>
+    <MissionBlock>
+      Users should be aware that providing information such as their public address, websites
+      visited, or other details may be personally identifiable. While we aim to prevent and/or
+      immediately destroy any irrelevant or sensitive personal information, it is at the User's sole
+      discretion to provide this information. Note that MyCrypto is not liable for any information
+      submitted by the User, and the User submits such information at her or his own risk.
+    </MissionBlock>
+    <MissionBlock>
+      If at any time you wish to have your Raw Report destroyed, please send a request to
+      contact@cryptoscamdb.org. The MyCrypto team will endeavor to destroyed in a timely manner.
     </MissionBlock>
   </Layout>
 );
