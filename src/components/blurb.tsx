98cd5b87e40d74e08c75306234591320ec1315cc
409H <409H@users.noreply.github.com>
2019-06-05 16:10:40 +0100

    Added blurb component

---
+++ src/components/blurb.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  blurbs: string[];
+}
+
+const BlurbContainer = styled.div`
+  border-left: 1px solid #fff;
+  padding-left: 1em;
+  margin-bottom: 3em;
+  width: 30%;
+`;
+const Segment = styled.p`
+  width: 100%;
+`;
+
+const Blurb: React.StatelessComponent<Props> = ({ blurbs }: Props) => (
+  <BlurbContainer>
+    {blurbs.map((b, k) => (
+      <Segment key={k}>{b}</Segment>
+    ))}
+  </BlurbContainer>
+);
+
+export default Blurb;
