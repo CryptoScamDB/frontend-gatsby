bc1962f35ed03033d71ab5f72a18afa05d77c7f0
409H <409H@users.noreply.github.com>
2019-06-13 16:35:45 +0100

    Refactor into styled-components

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
+import styled from 'styled-components';
+
 import Header from './Header';
 import '../css/layout.scss';
+
+const PageView = styled.div`
+  width: 100%;
+`;
+const Container = styled.div`
+  margin-top: 2em;
+  padding: 0em;
+`;
 
 interface Props {
   id?: any;
@@ -20,13 +30,13 @@ const Layout: React.StatelessComponent<Props> = ({ id, children }: Props) => (
       }
     `}
     render={data => (
-      <div id={id}>
+      <PageView id={id}>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div id={id + '--container'}>
+        <Container>
           {children}
           <footer />
-        </div>
-      </div>
+        </Container>
+      </PageView>
     )}
   />
 );
