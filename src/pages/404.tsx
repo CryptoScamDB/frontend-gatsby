76eb5c1368444f9b91c38a23a72d9f25b164fc1d
409H <409H@users.noreply.github.com>
2019-06-17 19:59:01 +0100

    Refactored to styled components and added media queries

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
+import styled from 'styled-components';
+
+import { Heading1 } from '../components/html/Headings';
+
+const Container = styled.div`
+  margin: 0 5%;
+  width: 90%;
+
+  @media (max-width: 968px) {
+    width: 100%;
+    margin: 0;
+  }
+`;
 
 const NotFound: React.StatelessComponent = () => (
   <Layout imageBg={false}>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <Heading1 text="NOT FOUND" />
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Container>
   </Layout>
 );
 
