52711b07eea94b7257f9656728983b6836a341bd
409H <409H@users.noreply.github.com>
2019-07-02 01:58:59 +0100

    Added unknown status logic

--- src/pages/scams.tsx
+++ src/pages/scams.tsx
@@ -179,7 +179,11 @@ const Scams: React.StatelessComponent<ScamsProps> = ({ data }: ScamsProps) => {
         default:
         case 'offline':
         case 'suspended':
-          objRecord.status = <ScamStatus status="inactive">{objRecord.status}</ScamStatus>;
+          objRecord.status = (
+            <ScamStatus status="inactive">
+              {objRecord.status === '' ? 'Unknown' : objRecord.status}
+            </ScamStatus>
+          );
           break;
       }
 
